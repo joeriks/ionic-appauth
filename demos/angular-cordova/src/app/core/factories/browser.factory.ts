b0e7b76fd91697696d18cd97b688945a94b69ee5
Jonas Eriksson <jonas@programenta.se>
2023-02-10 12:37:54 +0100

    browserfactory

--- demos/angular-cordova/src/app/core/factories/browser.factory.ts
+++ demos/angular-cordova/src/app/core/factories/browser.factory.ts
@@ -4,4 +4,5 @@ import { DefaultBrowser } from 'ionic-appauth';
 
 export let browserFactory = (platform: Platform) => {
     return platform.is('cordova') ? new CordovaBrowser() : new DefaultBrowser();
+    return new CapacitorBrowser();
 };
